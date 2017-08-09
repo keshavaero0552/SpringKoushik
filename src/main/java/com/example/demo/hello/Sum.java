947fde8d82d68a4b03028bd204da2b38976254b6
keshavaero0552 <keshav.aero0552@gmail.com>
2017-08-09 17:15:13 -0500

    just cleared some errors

--- src/main/java/com/example/demo/hello/Sum.java
+++ src/main/java/com/example/demo/hello/Sum.java
@@ -1,14 +1,19 @@
 package com.example.demo.hello;
 
 public class Sum {
-    public static void sumofNumbers()
-    {
+    public static void sumofNumbers() {
         System.out.println("printing sum");
     }
-    public static void NAme(){
+
+    public static void NAme() {
         System.out.println("Print my name");
     }
-    public static void run(){
-     System.out.println("printing deductions");
+
+    public static void run() {
+        System.out.println("printing deductions");
+    }
+
+    public static void runnning() {
+        System.out.println("printing deductions");
     }
 }
