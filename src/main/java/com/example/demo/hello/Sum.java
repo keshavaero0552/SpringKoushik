e3dcc97f37c50f7887b469799cc646bd5625b169
mounika <chirramounika286@gmail.com>
2017-08-09 16:39:05 -0500

    name change other method name

--- src/main/java/com/example/demo/hello/Sum.java
+++ src/main/java/com/example/demo/hello/Sum.java
@@ -3,5 +3,9 @@ package com.example.demo.hello;
 public class Sum {
     public static void sumofNumbers(){
         System.out.println("printing sum");
+
+    }
+    public static void NAme(){
+        System.out.println("Print my name");
     }
 }
